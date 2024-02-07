1110d683ec70e9eb967bf0c0e6f7e7127f4aa572
NSwanson216 <137955372+NSwanson216@users.noreply.github.com>
2024-02-07 15:54:17 -0800

    Update move_uneeded_folders.py

--- move_uneeded_folders.py
+++ move_uneeded_folders.py
@@ -5,12 +5,12 @@ import pandas as pd
 # double check this before running it, just to be safe
 
 if __name__ == '__main__':
-    csv_path = r'path\to\WGD\csv'
+    csv_path = r'/titan/cancerregulome10/workspaces/users/btercan/ForBahar/MILD/clinical_data/MILD_WGD_p-p.tsv'
     WGD_df = pd.read_csv(csv_path, sep=',')
     src = r'/titan/cancerregulome10/workspaces/users/btercan/ForBahar/MILD/tiles_FFPE'
     dest = r'/titan/cancerregulome10/workspaces/users/btercan/ForBahar/MILD/unused_tiles'
 
-    WGD_df.set_index('Sample ID', inplace=True)
+    WGD_df.set_index('Submitter ID', inplace=True)
     for name in os.listdir(src):
         full_path = os.path.join(src, name)
         if name not in WGD_df.index:
