2eacfd172843fc23c8ae3cb32e5362de827ea105
NSwanson216 <137955372+NSwanson216@users.noreply.github.com>
2024-02-07 15:45:15 -0800

    Update move_uneeded_folders.py

--- move_uneeded_folders.py
+++ move_uneeded_folders.py
@@ -7,8 +7,8 @@ import pandas as pd
 if __name__ == '__main__':
     csv_path = r'path\to\WGD\csv'
     WGD_df = pd.read_csv(csv_path, sep=',')
-    src = r'path\to\tile\folders'
-    dest = r'new\path\to\uneeded\folders'
+    src = r'/titan/cancerregulome10/workspaces/users/btercan/ForBahar/MILD/tiles_FFPE'
+    dest = r'/titan/cancerregulome10/workspaces/users/btercan/ForBahar/MILD/unused_tiles'
 
     WGD_df.set_index('Sample ID', inplace=True)
     for name in os.listdir(src):
