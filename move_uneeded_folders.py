52812e7de604085a0307663d96f9ed22299aeb54
NSwanson216 <137955372+NSwanson216@users.noreply.github.com>
2024-02-07 19:27:20 -0800

    Update move_uneeded_folders.py

--- move_uneeded_folders.py
+++ move_uneeded_folders.py
@@ -15,3 +15,41 @@ if __name__ == '__main__':
         full_path = os.path.join(src, name)
         if name not in WGD_df.index:
             shutil.move(full_path, dest)
+
+    MILD_path = r'/titan/cancerregulome10/workspaces/users/btercan/ForBahar/MILD'
+    subset_path = os.path.join(MILD_path, 'subsets')
+    if os.path.exists(subset_path) != True:
+        os.mkdir(subset_path)
+    new_dir = []
+    for i in range(1, 6):
+        subset_name = 'subset_' + str(i)
+        new_dir.append(os.path.join(subset_path, subset_name))
+        os.mkdir(new_dir[i-1])
+    i = 0
+    for name in os.listdir(src):
+        if i == 0:
+            full_path = os.path.join(src, name)
+            shutil.copy(full_path, new_dir[i])
+            i = 1
+        elif i == 1:
+            full_path = os.path.join(src, name)
+            shutil.copy(full_path, new_dir[i])
+            i = 2
+        elif i == 2:
+            full_path = os.path.join(src, name)
+            shutil.copy(full_path, new_dir[i])
+            i = 3
+        elif i == 3:
+            full_path = os.path.join(src, name)
+            shutil.copy(full_path, new_dir[i])
+            i = 4
+        elif i == 4:
+            full_path = os.path.join(src, name)
+            shutil.copy(full_path, new_dir[i])
+            i = 0
+
+
+
+
+
+        
